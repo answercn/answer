0dfa19e51994c3ff38a5b02c4be8891ff31b569d
hui.h.wang <hui.h.wang@DLC5CG6280F2PLH.dir.svc.accenture.com>
2017-12-24 19:34:50 +0800

    init

---
+++ answer.controller/src/main/java/com/dmai/answer/App.java
@@ -0,0 +1,13 @@
+package com.ali.seal.sealing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
